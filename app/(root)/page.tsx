f8f91ae4f52299c4d4141f4ec614ca3de879c33b
louisselynn26 <louisselynn26@gmail.com>
2024-11-08 02:44:00 +0800

    startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 
 export default async function Home({searchParams}: {
-  searchParams: Promise<{query?: string}>
-}) {
+  searchParams: Promise<{query?: string}>}) {
+
   const query =(await searchParams).query;
+
+  const posts = [{
+    _createdAt: Date(),
+    views: 55,
+    author: { _id: 1, name:'Lovi'},
+    _id: 1,
+    description: 'This is the description',
+    image: "https://www.posist.com/restaurant-times/wp-content/uploads/2016/10/A-Detailed-Guide-On-Starting-A-Bakery-Business-In-India-In-2023.jpg",
+    category: "Baked goods",
+    title: "Coco Bakery"
+  }]
+
   return (
     <>
       <section className="pink_container">
@@ -16,6 +29,25 @@ export default async function Home({searchParams}: {
         </p>
 
         <SearchForm query={query}/>
+      </section>
+
+      <section className="section-container">
+        <p className="text-30-semibold">
+          {query ? 
+          `Search results for "${query}"`
+          : 
+          'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          { posts?.length > 0 ? (
+              posts.map((post: StartupCardType, index: number) => (
+                <StartupCard key={post?._id} post={post}/>
+              ))
+          ): (
+            <p>No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   )
